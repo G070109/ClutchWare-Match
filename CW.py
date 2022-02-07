bf59031092ee9b3635a0a3404301b880e9b63b37
G070109 <98209070+G070109@users.noreply.github.com>
2022-02-07 02:57:04 -0600

    Update CW.py

--- CW.py
+++ CW.py
@@ -504,9 +504,9 @@ if b:
     m4.metric(label="Total", value=str(np.round(res[0], 3)), delta="Expected Points", delta_color="off")
     st.info("Recommended total line -"+t_line)
     st.info(Ml)
-    st.info(str(tm_A[0])+str(AVG_a)+"%")
-    st.info(AVG_h)
-    st.info(AVG_f)
+    st.info(str(tm_H[0])+" - "str(AVG_h)+"%")
+    st.info(str(tm_A[0])+" - "str(AVG_a)+"%")
+    st.info("Final % - "+ str(AVG_f))
 
     st.plotly_chart(tot_Graph_H)
     st.plotly_chart(tot_Graph_A)
