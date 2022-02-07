93e167f65decbbc5b86759044dfcdcce0c3f28e0
G070109 <98209070+G070109@users.noreply.github.com>
2022-02-07 02:53:25 -0600

    Update CW.py

--- CW.py
+++ CW.py
@@ -432,13 +432,13 @@ if b:
         line = Bt_ou_line - 5
         t_line = str('Over ' + str(line))
         AVG_h=OvPe(tot_H)
-        AVG_a=OvPe(tot_H)
+        AVG_a=OvPe(tot_A)
 
     else:
         line = Bt_ou_line + 5
         t_line = str('Under ' + str(line))
         AVG_h=UnPe(tot_H)
-        AVG_a=UnPe(tot_H)
+        AVG_a=UnPe(tot_A)
     
     AVG_f=(AVG_h+AVG_a)/2
 
@@ -504,9 +504,9 @@ if b:
     m4.metric(label="Total", value=str(np.round(res[0], 3)), delta="Expected Points", delta_color="off")
     st.info("Recommended total line -"+t_line)
     st.info(Ml)
-    st.info(str(tm_A[0]) + "-" +AVG_a)
-    st.info(str(tm_H[0] + "-" +AVG_h))
-    st.info(str("Total %" + "-" +AVG_f))
+    st.info(AVG_a)
+    st.info(AVG_h))
+    st.info(str(AVG_f)
 
     st.plotly_chart(tot_Graph_H)
     st.plotly_chart(tot_Graph_A)
