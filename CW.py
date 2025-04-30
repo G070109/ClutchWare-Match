f0750153a3b64aa9c4265765eb9412391b1c78b4
G070109 <98209070+G070109@users.noreply.github.com>
2025-04-30 02:53:47 -0600

    Update CW.py

--- CW.py
+++ CW.py
@@ -237,6 +237,8 @@ def tmnm(nm1):
         
         
 def AI(tm_pt, tm_av, opp_av):
+    tm_av = tm_av.reshape(-1, 1)
+    tm_pt = tm_pt.reshape(-1, 1)
     capa = tf.keras.layers.Dense(units=1, input_shape=[1])
     modelo = tf.keras.Sequential([capa])
 
