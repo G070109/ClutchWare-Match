26dd6138196cdaebc24fb9d127fda3ed471751ac
G070109 <98209070+G070109@users.noreply.github.com>
2025-04-30 03:14:20 -0600

    Update CW.py

--- CW.py
+++ CW.py
@@ -482,10 +482,10 @@ if b and ps=="wsa":
     
     with st.spinner("Starting training..."):
               
-                rHd = AI(H_pt,H_Def,Opp_A)
-                rAd = AI(A_pt,A_Def,Opp_H)
-                rHo = AI(H_pt,H_Def,Opp_H)
-                rAo = AI(A_pt,A_Def,Opp_A)
+                rHd = AI(H_pt, H_Def, Opp_H)
+                rAd = AI(A_pt, A_Def, Opp_A)
+                rHo = AI(H_oppt, H_Of, Opp_Ho)
+                rAo = AI(A_oppt, A_Of, Opp_Ao)
     st.success("Model trained!")
     resd = rHd + rAd
     reso = rHo + rAo 
